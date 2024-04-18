0765945790527378a62464ed5762568e4c4dfec8
michaelqu2 <165969833+michaelqu2@users.noreply.github.com>
2024-04-17 17:34:29 -0700

    Add Logging record and saved in shared preferences

--- lib/logging_display.dart
+++ lib/logging_display.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '/profile.dart';
 import '/initial_questions_golf.dart';
 import '/main.dart';
+import 'package:app_v1/logging_record.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoggingDisplayPage extends StatefulWidget {
   const LoggingDisplayPage({super.key});
@@ -11,6 +13,16 @@ class LoggingDisplayPage extends StatefulWidget {
 }
 
 class _LoggingDisplayPageState extends State<LoggingDisplayPage> {
+  String _sport = "";
+  late SharedPreferences prefs;
+
+  Future<void> _loadInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _sport = (prefs.getString("logSport") ?? '');
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +39,66 @@ class _LoggingDisplayPageState extends State<LoggingDisplayPage> {
                         children: [
                           Container(
                               width: 500,
-                              height: 200,
-                              child: Column(children: [
-                                Text(" LOG NUMBER"),
-                                TextButton(
-                                    onPressed: () {
-                                      print("add log");
-                                    },
-                                    child: const Text("Add Log"))
-                              ],)
-                          )
+                              height: 150,
+                              padding: const EdgeInsets.only(
+                                  left: 20, right: 20, top: 10, bottom: 20),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                      width: 150.0,
+                                      height: 200.0,
+                                      color: Colors.white,
+                                      child: Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text("LOG NUMBER"),
+                                          ])),
+                                  Container(
+                                      width: 100.0,
+                                      height: 50.0,
+                                      color: Colors.greenAccent,
+                                      child: Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            TextButton(
+                                                onPressed: () {
+                                                  print("add log");
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              const LoggingRecordPage()));
+                                                },
+                                                child: const Text("Add Log")),
+                                          ])),
+                                ],
+                              )),
+                          const Divider(
+                            height: 20,
+                            thickness: 2,
+                            indent: 10,
+                            endIndent: 0,
+                            color: Colors.black,
+                          ),
+                          Container(
+                              width: 500.0,
+                              height: 30.0,
+                              child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(_sport),
+
+                                  ])),
                         ],
                       ))))),
     );
---
+++ lib/logging_record.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import '/profile.dart';
+import '/initial_questions_golf.dart';
+import 'package:app_v1/logging_display.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class LoggingRecordPage extends StatefulWidget {
+  const LoggingRecordPage({super.key});
+
+  @override
+  State<LoggingRecordPage> createState() => _LoggingRecordPageState();
+}
+
+class _LoggingRecordPageState extends State<LoggingRecordPage> {
+  final _dateController = TextEditingController();
+  final _timeController = TextEditingController();
+  final _sportController = TextEditingController();
+  List<String> sports = [];
+  String? selected_sports;
+
+  Future<void> loadUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _dateController.text = prefs.getString("logDate") ?? '';
+      _timeController.text = prefs.getString("logTime") ?? '';
+      _sportController.text = prefs.getString("logSport") ?? '';
+      if (prefs.containsKey('sports')) sports = prefs.getStringList('sports')!;
+      selected_sports = sports.isNotEmpty ? sports.first : '';
+    });
+  }
+
+  Future<void> saveUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonlog = prefs.getString(selected_sports!)??'[]';
+    List logs = jsonDecode(jsonlog);
+    Map data = {
+      'logDate': _dateController.text,
+      'logTime': _timeController.text,
+      'logSport': _sportController.text,
+    };
+    logs.add(data);
+    prefs.setString(selected_sports!,jsonEncode(logs));
+    print("Log is added");
+  }
+  @override
+  void initState() {
+    super.initState();
+    loadUserInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                width: 500.0,
+                height: 100.0,
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Log #",
+                        style: TextStyle(
+                          fontSize: 24,
+                        ),
+                      ),
+                    ])),
+            const Divider(
+              height: 5,
+              thickness: 1,
+              indent: 0,
+              endIndent: 0,
+              color: Colors.black,
+            ),
+            Container(
+                width: 500.0,
+                height: 300.0,
+                child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          width: 300.0,
+                          height: 300.0,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 0),
+                                  child: TextField(
+                                    keyboardType: TextInputType.text,
+                                    decoration: InputDecoration(
+                                      labelText: "Date",
+                                      hintText: "Date",
+                                    ),
+                                    controller: _dateController,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 0),
+                                  child: TextField(
+                                    keyboardType: TextInputType.text,
+                                    decoration: InputDecoration(
+                                      labelText: "Time",
+                                      hintText: "Time",
+                                    ),
+                                    controller: _timeController,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 0),
+                                  child: TextField(
+                                    keyboardType: TextInputType.text,
+                                    decoration: InputDecoration(
+                                      labelText: "Sport",
+                                      hintText: "Sport",
+                                    ),
+                                    controller: _sportController,
+                                  ),
+                                )
+                              ])),
+                    ])),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                alignment: Alignment.center,
+              ),
+              onPressed: () {
+                print("save");
+                saveUserInfo();
+                Navigator.pop(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoggingDisplayPage()));
+              },
+              child: const Text(
+                "Save",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
